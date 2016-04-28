a5b50a6404d66e0878ef60539683635075087418
HIDEKUNI Kajita <hide.nba@gmail.com>
2016-04-28 18:45:42 +0900

    telling the time

--- src/watchface.c
+++ src/watchface.c
@@ -3,6 +3,20 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
+static void update_time() {
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style()? "%H:%M" : "%l:%M", tick_time);
+
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
 static void main_window_load(Window *window){
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
@@ -15,6 +29,8 @@ static void main_window_load(Window *window){
   text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
@@ -33,6 +49,8 @@ static void init() {
   window_set_window_handlers(s_main_window, handlers);
 
   window_stack_push(s_main_window, true);
+
+  update_time();
 }
 
 static void deinit() {
